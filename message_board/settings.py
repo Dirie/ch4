906697d02faaebe9222b187a63b5196438c23fe4
Dirie <abdulrazak.dirie@gmail.com>
2022-10-18 23:23:07 +0300

    allow urls in settings file is updated

--- message_board/settings.py
+++ message_board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i7^u+9$2j^8y-mn)qtb--%&x!=yrm6p8c&9rg*=se0w$#505ae
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".up.railway.app", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["web-production-a836.up.railway.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
