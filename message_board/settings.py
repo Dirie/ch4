a393a98cc7f51bab9fda853a04df4a1bad00bc55
Dirie <abdulrazak.dirie@gmail.com>
2022-10-18 23:14:42 +0300

    allow urls in settings file is updated

--- message_board/settings.py
+++ message_board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i7^u+9$2j^8y-mn)qtb--%&x!=yrm6p8c&9rg*=se0w$#505ae
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
